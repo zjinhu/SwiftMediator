dcab7dd7493b0fb4900aa423903e84efcd775795
Jackie <jackie>
2020-10-26 16:27:01 +0800

    release

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftMediator",
-            path: "SwiftMediator/SwiftMediator/Class")
+            dependencies: []),
     ],
     swiftLanguageVersions: [
         .v5
