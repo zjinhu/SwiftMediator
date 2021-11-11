61afe29ebe780ec4106a2d3b5cb349c025d626fc
Jackie <jackie>
2021-11-11 11:12:27 +0800

    更新demo备注用法

--- SwiftMediator/SwiftMediator/ViewController.swift
+++ SwiftMediator/SwiftMediator/ViewController.swift
@@ -13,31 +13,68 @@ class ViewController: JHTableViewController {
     
     lazy var former = Former(tableView: self.tableView!)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.title = "路由示例"
-
+    lazy var sectionFormer1 : SectionFormer = {
         let sectionFormer = SectionFormer(row1,
-                                          row2,
-                                          row3,
+                                          row2)
+        let header = LabelHeaderFooter()
+        header.viewHeight = 20
+        header.title = "present页面,可选导航栏,模态样式"
+        header.headerFooter.backColor = .baseTeal
+        sectionFormer.set(headerViewFormer: header)
+        return sectionFormer
+    }()
+    
+    lazy var sectionFormer2 : SectionFormer = {
+        let sectionFormer = SectionFormer(row3,
                                           row4,
-                                          row5,
-                                          row6,
+                                          row5)
+        let header = LabelHeaderFooter()
+        header.viewHeight = 20
+        header.title = "push页面,可关闭动画"
+        header.headerFooter.backColor = .baseTeal
+        sectionFormer.set(headerViewFormer: header)
+        return sectionFormer
+    }()
+    
+    lazy var sectionFormer3 : SectionFormer = {
+        let sectionFormer = SectionFormer(row6,
                                           row7,
-                                          row8,
-                                          row13,
-                                          row9,
-                                          row10,
+                                          row8)
+        let header = LabelHeaderFooter()
+        header.viewHeight = 20
+        header.title = "URL跳转页面,可选push,present"
+        header.headerFooter.backColor = .baseTeal
+        sectionFormer.set(headerViewFormer: header)
+        return sectionFormer
+    }()
+    
+    lazy var sectionFormer4 : SectionFormer = {
+        let sectionFormer = SectionFormer(row10,
                                           row11,
                                           row12)
-        former.append(sectionFormer: sectionFormer)
-    }
+        let header = LabelHeaderFooter()
+        header.viewHeight = 20
+        header.title = "其他方法函数调用"
+        header.headerFooter.backColor = .baseTeal
+        sectionFormer.set(headerViewFormer: header)
+        return sectionFormer
+    }()
+    
+    lazy var sectionFormer5 : SectionFormer = {
+        let sectionFormer = SectionFormer(row9,
+                                          row13)
+        let header = LabelHeaderFooter()
+        header.viewHeight = 20
+        header.title = "跨组件调用"
+        header.headerFooter.backColor = .baseTeal
+        sectionFormer.set(headerViewFormer: header)
+        return sectionFormer
+    }()
     
     lazy var row1 : LabelRow = {
         let row = LabelRow()
         row.title = "present页面"
-        row.subTitle = "用法1"
+        row.subTitle = "默认用法,添加导航栏"
         row.cell.addDownLine()
         row.cell.backgroundColor = .baseBGColor
         row.cell.accessoryType = .disclosureIndicator
@@ -48,7 +85,7 @@ class ViewController: JHTableViewController {
                                                                "num":13,
                                                                "dic":["a":12,
                                                                       "b":"测试字符串"]
-            ])
+                                                              ])
             
         }
         return row
@@ -57,7 +94,7 @@ class ViewController: JHTableViewController {
     lazy var row2 : LabelRow = {
         let row = LabelRow()
         row.title = "present页面"
-        row.subTitle = "用法2"
+        row.subTitle = "可选模态样式,导航栏等"
         row.cell.addDownLine()
         row.cell.backgroundColor = .baseBGColor
         row.cell.accessoryType = .disclosureIndicator
@@ -76,7 +113,7 @@ class ViewController: JHTableViewController {
     lazy var row3 : LabelRow = {
         let row = LabelRow()
         row.title = "push页面"
-        row.subTitle = "用法1"
+        row.subTitle = "手动初始化push"
         row.cell.addDownLine()
         row.cell.backgroundColor = .baseBGColor
         row.cell.accessoryType = .disclosureIndicator
@@ -95,7 +132,7 @@ class ViewController: JHTableViewController {
     lazy var row4 : LabelRow = {
         let row = LabelRow()
         row.title = "push页面"
-        row.subTitle = "用法2"
+        row.subTitle = "默认用法"
         row.cell.addDownLine()
         row.cell.backgroundColor = .baseBGColor
         row.cell.accessoryType = .disclosureIndicator
@@ -113,7 +150,7 @@ class ViewController: JHTableViewController {
     lazy var row5 : LabelRow = {
         let row = LabelRow()
         row.title = "push页面"
-        row.subTitle = "用法3"
+        row.subTitle = "可选动画,出发页面等"
         row.cell.addDownLine()
         row.cell.backgroundColor = .baseBGColor
         row.cell.accessoryType = .disclosureIndicator
@@ -123,8 +160,8 @@ class ViewController: JHTableViewController {
                                                                   "titleName":"push页面3",
                                                                   "num":13,
                                                                   "dic":["a":12,"b":"jlj"]])
-//            self.navigationController?.pushViewController(avc!, animated: true)
-            SwiftMediator.shared.push(avc)
+            //            self.navigationController?.pushViewController(avc!, animated: true)
+            SwiftMediator.shared.push(avc, animated: false)
             
         }
         return row
@@ -133,7 +170,7 @@ class ViewController: JHTableViewController {
     lazy var row6 : LabelRow = {
         let row = LabelRow()
         row.title = "URL跳转页面"
-        row.subTitle = "用法1"
+        row.subTitle = "push"
         row.cell.addDownLine()
         row.cell.backgroundColor = .baseBGColor
         row.cell.accessoryType = .disclosureIndicator
@@ -148,7 +185,7 @@ class ViewController: JHTableViewController {
     lazy var row7 : LabelRow = {
         let row = LabelRow()
         row.title = "URL跳转页面"
-        row.subTitle = "用法2"
+        row.subTitle = "present"
         row.cell.addDownLine()
         row.cell.backgroundColor = .baseBGColor
         row.cell.accessoryType = .disclosureIndicator
@@ -163,7 +200,7 @@ class ViewController: JHTableViewController {
     lazy var row8 : LabelRow = {
         let row = LabelRow()
         row.title = "URL跳转页面"
-        row.subTitle = "用法3"
+        row.subTitle = "present,全屏"
         row.cell.addDownLine()
         row.cell.backgroundColor = .baseBGColor
         row.cell.accessoryType = .disclosureIndicator
@@ -178,7 +215,7 @@ class ViewController: JHTableViewController {
     lazy var row13 : LabelRow = {
         let row = LabelRow()
         row.title = "URL跳转页面"
-        row.subTitle = "用法4"
+        row.subTitle = "打开其他组件的网页H5"
         row.cell.addDownLine()
         row.cell.backgroundColor = .baseBGColor
         row.cell.accessoryType = .disclosureIndicator
@@ -225,7 +262,7 @@ class ViewController: JHTableViewController {
     lazy var row11 : LabelRow = {
         let row = LabelRow()
         row.title = "实例方法调用"
-        row.subTitle = "用法1"
+        row.subTitle = "可拿实例函数的回传参数"
         row.cell.addDownLine()
         row.cell.backgroundColor = .baseBGColor
         row.cell.accessoryType = .disclosureIndicator
@@ -238,7 +275,7 @@ class ViewController: JHTableViewController {
         }
         return row
     }()
-
+    
     lazy var row12 : LabelRow = {
         let row = LabelRow()
         row.title = "实例方法调用"
@@ -255,5 +292,17 @@ class ViewController: JHTableViewController {
         }
         return row
     }()
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "路由示例"
+        
+        former.append(sectionFormer: sectionFormer1,
+                      sectionFormer2,
+                      sectionFormer3,
+                      sectionFormer4,
+                      sectionFormer5)
+    }
+    
 }
