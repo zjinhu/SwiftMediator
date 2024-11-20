87d1c00c2c8b8511812047aa71a332a04b09c733
虎 <814030966@qq.com>
2024-11-20 10:29:08 +0800

    添加initView

--- Sources/SwiftMediator/Target-Action/Init++.swift
+++ Sources/SwiftMediator/Target-Action/Init++.swift
@@ -60,4 +60,30 @@ extension SwiftMediator {
         setObjectParams(obj: objc, paramsDic: dic)
         return objc
     }
+    
+    /// Reflect UIView initialization and assignment
+    /// - Parameters:
+    /// - moduleName: component boundle name, if not passed, it will be the default namespace
+    /// - vcName: UIView name
+    /// - dic: parameter dictionary // Since it is a KVC assignment, @objc must be marked on the parameter
+    @discardableResult
+    public func initView(_ viewName: String,
+                       moduleName: String? = nil,
+                       dic: [String: Any]? = nil) -> UIView?{
+        
+        var namespace = Bundle.main.infoDictionary!["CFBundleExecutable"] as! String
+        if let name = moduleName {
+            namespace = name
+        }
+        
+        let className = "\(namespace).\(viewName)"
+        let cls: AnyClass? = NSClassFromString(className)
+        guard let vc = cls as? UIView.Type else {
+            return nil
+        }
+        let view = vc.init()
+        setObjectParams(obj: view, paramsDic: dic)
+        return view
+    }
+
 }
